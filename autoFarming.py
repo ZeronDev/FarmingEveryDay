e5ab0ff5941ac63bb2262027bc7dc8ecf562b441
ZeronDev <zeron_0304@naver.com>
2023-04-28 22:28:11 +0900

    1

--- autoFarming.py
+++ autoFarming.py
@@ -1,26 +1,20 @@
-import datetime
-import schedule
 import time
 import os
-# from keep_alive import keep_alive
 
 counter = 0
 
 def job():
   global counter
-  date = datetime.datetime.now()
 
   with open("./README.md", "a", encoding="utf-8") as readme:
       readme.write(" ")
 
   os.system("git add README.md")
-  os.system(f"git commit -m \"{date.year}/{date.month}/{date.day}\"")
+  os.system(f"git commit -m \"{counter}\"")
   os.system("git push origin main")
     
   counter += 1
 
-schedule.every(1).seconds.do(job)
-
 while True:
-    schedule.run_pending()
+    job()
     time.sleep(1)
